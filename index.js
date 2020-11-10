d9f95497a3e74cf0430fd81f26644ba15cc371a1
Soakmamank <62498428+Soakmamank@users.noreply.github.com>
2020-11-10 23:14:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'G-SOA BOT☄'; 
+const BotName = 'G-SOA BOT'; 
 const instagram = 'https://instagram.com/masge_24'; 
 const telegram = 'https://t.me/Mz.G-SOA'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
